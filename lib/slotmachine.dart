a290db00ba9ede127389d8e5dade63328dc7255b
nikitosoj <nikita.kaneki@gmail.com>
2024-05-20 13:37:33 +0300

    add custom random

--- lib/slotmachine.dart
+++ lib/slotmachine.dart
@@ -33,11 +33,11 @@ class _SlotMachineState extends State<SlotMachine> {
     "redJohn"
   ];
   int? first, second, third, fourth;
-  final leftRoller = new GlobalKey<RollerListState>();
-  final centerRoller = new GlobalKey<RollerListState>();
-  final rightRoller = new GlobalKey<RollerListState>();
+  final leftRoller = GlobalKey<RollerListState>();
+  final centerRoller = GlobalKey<RollerListState>();
+  final rightRoller = GlobalKey<RollerListState>();
   Timer? rotator;
-  Random _random = new Random();
+  final Random _random = Random();
   int? randomRotationTimeInSeconds;
   DateTime? startRotation;
 
@@ -100,6 +100,8 @@ class _SlotMachineState extends State<SlotMachine> {
                           onSelectedIndexChanged: (value) {
                             setState(() {
                               first = value - 1;
+                              print(
+                                  'first = $first'); // забирать выигранное значение
                             });
                           },
                         ),
@@ -117,7 +119,9 @@ class _SlotMachineState extends State<SlotMachine> {
                           key: centerRoller,
                           onSelectedIndexChanged: (value) {
                             setState(() {
-                              third = value - 1;
+                              second = value - 1;
+                              print(
+                                  'second $second'); // забирать выигранное значение
                             });
                           },
                         ),
@@ -135,7 +139,9 @@ class _SlotMachineState extends State<SlotMachine> {
                           key: rightRoller,
                           onSelectedIndexChanged: (value) {
                             setState(() {
-                              fourth = value - 1;
+                              third = value - 1;
+                              print(
+                                  'third $third'); // забирать выигранное значение
                             });
                           },
                         ),
@@ -159,11 +165,6 @@ class _SlotMachineState extends State<SlotMachine> {
             ),
           ),
         ),
-        if (slotNames.length < third!)
-          Text(
-              "Result: ${slotNames[first!]}-${slotNames[second!]}-${slotNames[third!]}"),
-        if (slotNames.length < third!)
-          (first == second && first == third) ? Text("WIN!!!") : Container(),
       ],
     );
   }
@@ -182,27 +183,26 @@ class _SlotMachineState extends State<SlotMachine> {
     if (DateTime.now().difference(startRotation!).inSeconds >
         randomRotationTimeInSeconds! + 3) {
       finish = true;
+
       animateController.stop();
       _finishRotating();
     } else if (DateTime.now().difference(startRotation!).inSeconds >
         randomRotationTimeInSeconds! - 3) {
-      final leftRotationTarget = _random.nextInt(3 * slots.length);
+      final leftRotationTarget = _getWeightedRandomSlotIndex();
+      final centerRotationTarget = _getWeightedRandomSlotIndex();
+      final rightRotationTarget = _getWeightedRandomSlotIndex();
 
-      final centerLeftRotationTarget = _random.nextInt(3 * slots.length);
-      final centerRightRotationTarget = _random.nextInt(3 * slots.length);
-      // print('first object = $centerLeftRotationTarget'); //забирать
-
-      leftRoller.currentState?.smoothScrollToIndex(centerLeftRotationTarget,
-          duration: _rotationTimeStep2, curve: Curves.decelerate);
-      centerRoller.currentState?.smoothScrollToIndex(centerRightRotationTarget,
-          duration: _rotationTimeStep3, curve: Curves.decelerate);
-      rightRoller.currentState?.smoothScrollToIndex(leftRotationTarget,
+      leftRoller.currentState?.smoothScrollToIndex(centerRotationTarget,
           duration: _rotationTimeStep, curve: Curves.decelerate);
+      centerRoller.currentState?.smoothScrollToIndex(rightRotationTarget,
+          duration: _rotationTimeStep2, curve: Curves.decelerate);
+      rightRoller.currentState?.smoothScrollToIndex(leftRotationTarget,
+          duration: _rotationTimeStep3, curve: Curves.decelerate);
     } else {
-      final leftRotationTarget = _random.nextInt(3 * slots.length);
+      final leftRotationTarget = _getWeightedRandomSlotIndex();
 
-      final centerLeftRotationTarget = _random.nextInt(3 * slots.length);
-      final centerRightRotationTarget = _random.nextInt(3 * slots.length);
+      final centerLeftRotationTarget = _getWeightedRandomSlotIndex();
+      final centerRightRotationTarget = _getWeightedRandomSlotIndex();
 
       leftRoller.currentState?.smoothScrollToIndex(centerLeftRotationTarget,
           duration: _rotationTimeStep, curve: Curves.linear);
@@ -223,7 +223,7 @@ class _SlotMachineState extends State<SlotMachine> {
     for (int i = 0; i <= 9; i++) {
       result.add(Container(
         padding: EdgeInsets.all(4.0),
-        color: Colors.white,
+        color: Colors.red,
         child: SvgPicture.asset(
           "assets/hiQ_numbers/DarkGraffiti_$i.svg",
           width: double.infinity,
@@ -231,7 +231,39 @@ class _SlotMachineState extends State<SlotMachine> {
         ),
       ));
     }
-    result.add(SvgPicture.asset("assets/red_john.svg"));
+    result.add(Container(
+      padding: EdgeInsets.all(4),
+      color: Colors.red,
+      child: SvgPicture.asset(
+        "assets/red_john.svg",
+      ),
+    ));
     return result;
+  }
+
+  int _getWeightedRandomSlotIndex() {
+    final double targetProbability =
+        0.05; // 5% вероятность для последнего слота
+    final int numSlots = slots.length;
+    final double remainingProbability = 1.0 - targetProbability;
+    final double slotProbability = remainingProbability / (numSlots - 1);
+
+    // Генерируем случайное число от 0 до 1
+    final double randomValue = _random.nextDouble();
+
+    if (randomValue < targetProbability) {
+      return numSlots; // Последний слот
+    } else {
+      // Оставшиеся слоты
+      double cumulativeProbability = targetProbability;
+      for (int i = 0; i < numSlots - 1; i++) {
+        cumulativeProbability += slotProbability;
+        if (randomValue < cumulativeProbability) {
+          return i;
+        }
+      }
+    }
+
+    return numSlots - 1; // fallback на последний слот
   }
 }
